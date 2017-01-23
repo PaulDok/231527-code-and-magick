1d067fa5967cf7687003802ba882ba91e519dd98
PaulDok <pauldok@rambler.ru>
2017-01-23 20:43:00 +0300

    Правки по комментариям

--- js/stat.js
+++ js/stat.js
@@ -2,13 +2,9 @@
 
 window.renderStatistics = function (ctx, names, times) {
   // draw a shadow
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7';
-  ctx.fillRect(110, 20, 420, 270);
-
+  drawRect(ctx, 110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)', 'rgba(0, 0, 0, 0.7)');
   // draw a white rectangle
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  drawRect(ctx, 100, 10, 420, 270, 'rgba(256, 256, 256, 1.0)', 'rgba(0, 0, 0, 0.7)');
 
   // print header text
   ctx.fillStyle = '#000';
@@ -17,6 +13,47 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, 60);
 
   // find max result to scale the columns
+  var maxTime = getMaxTime(times);
+
+  // variables for histogram
+  var histoHeight = 150;
+  var histoX = 120;
+  var histoBottomY = 100 + histoHeight;
+  var scale = histoHeight / maxTime;
+  var columnWidth = 40;
+  var columnSpacing = 50;
+
+  // draw individual results in a loop
+  for (var i = 0; i < times.length; i++) {
+    var name = names[i];
+    var time = times[i];
+    var height = scale * time;
+    drawColumnAndScore(ctx, i, name, time, histoX, histoBottomY, columnWidth, columnSpacing, height);
+  }
+};
+
+var drawRect = function (ctx, x, y, width, height, backgroundColor, borderColor) {
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(x, y, width, height);
+  ctx.fillStyle = borderColor;
+  ctx.strokeRect(x, y, width, height);
+};
+
+var drawColumnAndScore = function (ctx, i, name, time, histoX, histoBottomY, columnWidth, columnSpacing, height) {
+  var x = histoX + i * (columnWidth + columnSpacing);
+  var y = histoBottomY - height;
+
+  // draw column
+  var fillColor = getRandomColor (name);
+  drawRect(ctx, x, y, columnWidth, height, fillColor, 'rgba(0, 0, 0, 0.7)');
+
+  // print name and score
+  ctx.fillStyle = '#000';
+  ctx.fillText(time.toFixed(0), x, y - 5);
+  ctx.fillText(name, x, histoBottomY + 20);
+};
+
+var getMaxTime = function (times) {
   var maxTime = -1;
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -24,35 +61,17 @@ window.renderStatistics = function (ctx, names, times) {
       maxTime = time;
     }
   }
-  // variables for histogram
-  var histoHeight = 150;
-  var histoX = 120;
-  var histoBottomY = 100 + histoHeight;
-  var histoTextY = histoBottomY + 20;
-  var scale = histoHeight / maxTime;
-  var columnWidth = 40;
-  var columnSpacing = 50;
+  return maxTime;
+};
 
-  // draw individual results in a loop
-  for (i = 0; i < times.length; i++) {
-    var name = names[i];
-    time = times[i];
-    var height = scale * time;
-
-    if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      // Поскольку в задании сказано
-      // Цвет колонки других игроков — синие, а насыщенность задается случайным образом
-      // то для управления _насыщенностью_ использован формат hsl
-      ctx.fillStyle = 'hsl(240,' + Math.floor(Math.random() * 100) + '%,50%)';
-    }
-    ctx.fillRect(histoX + i * (columnWidth + columnSpacing),
-        histoBottomY - height,
-        columnWidth,
-        height);
-    ctx.fillStyle = '#000';
-    ctx.fillText(time.toFixed(0), histoX + i * (columnWidth + columnSpacing), histoBottomY - height - 5);
-    ctx.fillText(name, histoX + i * (columnWidth + columnSpacing), histoTextY);
+var getRandomColor = function (name) {
+  if (name === 'Вы') {
+    return 'rgba(255, 0, 0, 1)';
+  } else {
+    // Поскольку в задании сказано
+    // Цвет колонки других игроков — синие, а насыщенность задается случайным образом
+    // то для управления _насыщенностью_ использован формат hsl
+    var saturation = Math.floor(Math.random() * 100);
+    return 'hsl(240,' + saturation + '%,50%)';
   }
 };
