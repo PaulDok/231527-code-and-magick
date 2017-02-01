93356fc77e3461e5e4187cf93d4153677a23ecfa
PaulDok <pauldok@rambler.ru>
2017-02-01 21:05:55 +0300

    Нажатие посохом

--- js/setup.js
+++ js/setup.js
@@ -4,21 +4,60 @@
 var setupOverlay = document.getElementsByClassName('setup')[0];
 var setupOpenButton = document.getElementsByClassName('setup-open')[0];
 var setupCloseButton = setupOverlay.getElementsByClassName('setup-close')[0];
+var setupSubmitButton = setupOverlay.getElementsByClassName('setup-submit')[0];
 var coat = document.getElementById('wizard-coat');
 var eyes = document.getElementById('wizard-eyes');
 var fireball = setupOverlay.getElementsByClassName('setup-fireball-wrap')[0];
 
+var ENTER_KEY_CODE = 13;
+var ESCAPE_KEY_CODE = 27;
+
 // Setup form open/close handlers
-var showSetupHandler = function () {
+var showSetup = function () {
   setupOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', setupKeydownHandler);
+  setupOpenButton.setAttribute("aria-pressed", true);
 };
 
-var hideSetupHandler = function () {
+var hideSetup = function () {
   setupOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', setupKeydownHandler);
+  setupOpenButton.setAttribute("aria-pressed", false);
 };
 
-setupOpenButton.addEventListener('click', showSetupHandler);
-setupCloseButton.addEventListener('click', hideSetupHandler);
+var isActivateEvent = function (event) {
+  return event.keyCode && event.keyCode === ENTER_KEY_CODE;
+};
+
+var isEscapeEvent = function (event) {
+  return event.keyCode && event.keyCode === ESCAPE_KEY_CODE;
+};
+
+var setupKeydownHandler = function (event) {
+  if (event.target !== document.querySelector('input') && isEscapeEvent(event)) {
+    hideSetup();
+  }
+};
+
+var showSetupOnEnterHandler = function (event) {
+  if (isActivateEvent(event)) {
+    showSetup();
+  }
+};
+
+var hideSetupOnEnterHandler = function (event) {
+  if (isActivateEvent(event)) {
+    hideSetup();
+  }
+};
+
+setupOpenButton.addEventListener('click', showSetup);
+setupCloseButton.addEventListener('click', hideSetup);
+setupSubmitButton.addEventListener('click', hideSetup);
+
+setupOpenButton.addEventListener('keydown', showSetupOnEnterHandler);
+setupCloseButton.addEventListener('keydown', hideSetupOnEnterHandler);
+setupSubmitButton.addEventListener('keydown', hideSetupOnEnterHandler);
 
 // Color changing
 var coatColorOptions = [
