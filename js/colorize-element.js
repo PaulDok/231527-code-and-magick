be3e84acaaaf1d6d80131a6bad0165d14d03a2ed
PaulDok <pauldok@rambler.ru>
2017-02-15 21:50:22 +0300

    Полная изоляция, учебный проект

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,24 +1,27 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, property) {
-  var currentPropertyValue = element.style[property];
+window.colorizeElement = (function () {
   var ENTER_KEY_CODE = 13;
 
   var isActivateEvent = function (event) {
     return event && event.keyCode === ENTER_KEY_CODE;
   };
 
-  var randomizeElementColor = function () {
-    currentPropertyValue = window.utils.getRandomElementExcept(colors, currentPropertyValue);
-    element.style[property] = currentPropertyValue;
-  };
+  return function (element, colors, property) {
+    var currentPropertyValue = element.style[property];
 
-  var randomizeElementColorOnEnter = function (event) {
-    if (isActivateEvent(event)) {
-      randomizeElementColor();
-    }
-  };
+    var randomizeElementColor = function () {
+      currentPropertyValue = window.utils.getRandomElementExcept(colors, currentPropertyValue);
+      element.style[property] = currentPropertyValue;
+    };
 
-  element.addEventListener('click', randomizeElementColor);
-  element.addEventListener('keydown', randomizeElementColorOnEnter);
-};
+    var randomizeElementColorOnEnter = function (event) {
+      if (isActivateEvent(event)) {
+        randomizeElementColor();
+      }
+    };
+
+    element.addEventListener('click', randomizeElementColor);
+    element.addEventListener('keydown', randomizeElementColorOnEnter);
+  };
+})();
